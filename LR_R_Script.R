e7c425cdbbfdd4c6bae097d76a9dff4637c40147
deepthi <deepstein2003@gmail.com>
2014-04-06 18:46:42 +0800

    final changes

--- LR_R_Script.R
+++ LR_R_Script.R
@@ -1,69 +1,55 @@
-#coefficients(player_sal_reg)
-#confint(player_sal_reg, level=0.95) # CIs for model parameters 
-#fitted(player_sal_reg) # predicted values
-#residuals(player_sal_reg) # residuals
-#anova(player_sal_reg) # anova table 
-#vcov(player_sal_reg) # covariance matrix for model parameters 
-#influence(player_sal_reg) # regression diagnostics
+
 ####ENV CONFIG#######
-library(ggplot2)
-library(plm)
-install.packages('plm')
+#library(ggplot2)
+#install.packages('plm')
+#library(car)
 #Read the player salary data here
-PLOT_DIR<-'~/GP2/GP2/plots'
+PLOT_DIR<-'~/GP2/GP2/plots/regression'
 #####################
 
 ######################################################################################################
 #Player regresion analysis on salary vs points and Field goal parameters
 player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
-player_sal_reg <- lm(player_salary$salary ~ player_salary$PLAYER_SEASON_STATS_PTS 
-                     + player_salary$PLAYER_SEASON_STATS_TRB
-                     + player_salary$PLAYER_SEASON_STATS_AST
+player_sal_reg1 <- lm(player_salary$salary ~ player_salary$AGE 
+                     + player_salary$PTS
+                     + player_salary$TRB
                     , player_salary)
-summary(player_sal_reg)
+summary(player_sal_reg1)
 #Plot the trend of FGA vs salary
-png(paste(PLOT_DIR, '/Multiple-Salary.png', sep=''))
+png(paste(PLOT_DIR, '/PTS_TRB_AGE-Salary.png', sep=''))
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page 
-plot(player_sal_reg)
-
-#abline(player_sal_reg)
+plot(player_sal_reg1)
 dev.off()
 ######################################################################################################
 
 ######################################################################################################
 #Player regresion analysis on salary vs points and Field goal parameters
 player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
-player_sal_reg <- lm(player_salary$salary ~ 
+player_sal_reg2 <- lm(player_salary$salary ~ 
                        player_salary$AGE
-                       + player_salary$PLAYER_SEASON_STATS_STL
-                       + player_salary$PLAYER_SEASON_STATS_BLK, 
+                       + player_salary$THREE_P
+                       + player_salary$FG, 
                      player_salary)
-summary(player_sal_reg)
+summary(player_sal_reg2)
 #Plot the trend of FGA vs salary
-png(paste(PLOT_DIR, '/STL_BLK_AGE-Salary.png', sep=''))
+png(paste(PLOT_DIR, '/AGE_THREEP_FG-Salary.png', sep=''))
 layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page 
-plot(player_sal_reg)
+plot(player_sal_reg2)
 dev.off()
-png(paste(PLOT_DIR, '/STL_BLK_AGE_Influence-Salary.png', sep=''))
-#avPlots(player_sal_reg, id.n=2, id.cex=0.7)
-showLabels
-outlierTest(player_sal_reg)
-influencePlot(player_sal_reg, id.n=3)
-#qqplot(resid(player_sal_reg), id.method = "identify")
-dev.off()
+anova(player_sal_reg2, test='F')
 ######################################################################################################
 
 ######################################################################################################
 #Player regresion analysis on salary vs points parameter
 player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
 player_sal_reg <- lm(player_salary$salary ~ 
-                       player_salary$PLAYER_SEASON_STATS_PTS, 
+                       player_salary$PTS, 
                      player_salary)
 summary(player_sal_reg)
 
 #Plot the trend of PTS vs salary
 png(paste(PLOT_DIR, '/PTS-Salary.png' , sep=''))
-plot(player_salary$PLAYER_SEASON_STATS_PTS, player_salary$salary, xlab='POINTS',ylab='Salary')
+plot(player_salary$PTS, player_salary$salary, xlab='POINTS',ylab='Salary')
 abline(player_sal_reg)
 dev.off()
 ######################################################################################################
@@ -72,13 +58,13 @@ dev.off()
 #Player regresion analysis on salary vs FGP parameter
 player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
 player_sal_reg <- lm(player_salary$salary ~ 
-                       player_salary$PLAYER_SEASON_STATS_FGP, 
+                       player_salary$FG, 
                      player_salary)
 summary(player_sal_reg)
 
 #Plot the trend of PTS vs salary
 png(paste(PLOT_DIR, '/FGP-Salary.png' , sep=''))
-plot(player_salary$PLAYER_SEASON_STATS_FGP, player_salary$salary, xlab='FGP',ylab='Salary')
+plot(player_salary$FG, player_salary$salary, xlab='FGP',ylab='Salary')
 abline(player_sal_reg)
 dev.off()
 ######################################################################################################
@@ -86,12 +72,12 @@ dev.off()
 ######################################################################################################
 #Player regresion analysis on Salary vs Rebounds parameter
 player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
-player_sal_reg <- lm(player_salary$salary ~ player_salary$PLAYER_SEASON_STATS_TRB, player_salary)
+  player_sal_reg <- lm(player_salary$salary ~ player_salary$THREE_P, player_salary)
 summary(player_sal_reg)
 
 #Plot the trend of Rebounds vs salary
-png(paste(PLOT_DIR,'/TRB-Salary.png' , sep=''))
-plot(player_salary$PLAYER_SEASON_STATS_TRB, player_salary$salary, xlab='Defensive + Offensive Rebounds',ylab='Salary')
+png(paste(PLOT_DIR,'/THREEP-Salary.png' , sep=''))
+plot(player_salary$THREE_P, player_salary$salary, xlab='3p',ylab='Salary')
 anova(player_sal_reg) # anova table
 abline(player_sal_reg)
 dev.off()
@@ -100,12 +86,12 @@ dev.off()
 ######################################################################################################
 #Player regresion analysis on Salary vs Height parameter
 player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
-player_sal_reg <- lm(player_salary$salary ~ player_salary$HEIGHT, player_salary)
+player_sal_reg <- lm(player_salary$salary ~ player_salary$TRB, player_salary)
 summary(player_sal_reg)
 
 #Plot the trend of Rebounds vs salary
-png(paste(PLOT_DIR,'/Height-Salary.png' , sep=''))
-plot(player_salary$HEIGHT, player_salary$salary, xlab='Height',ylab='Salary')
+png(paste(PLOT_DIR,'/TRB-Salary.png' , sep=''))
+plot(player_salary$TRB, player_salary$salary, xlab='TRB',ylab='Salary')
 anova(player_sal_reg) # anova table
 abline(player_sal_reg)
 dev.off()
@@ -135,43 +121,5 @@ png(paste(PLOT_DIR, '/Age-Salary.png' , sep=''))
 plot(player_salary$AGE, player_salary$salary, xlab='Age',ylab='Salary')
 anova(player_sal_reg) # anova table
 abline(player_sal_reg)
-dev.off()
-######################################################################################################
-
-######################################################################################################
-#Player regresion analysis on Salary vs AGE+Experience parameter
-player_salary <- read.csv("~/GP2/GP2/player_salary.csv")
-  player_sal_reg <- lm(player_salary$salary ~ player_salary$AGE + player_salary$PLAYER_SEASON_STATS_PTS, player_salary)
-summary(player_sal_reg)
-
-#Plot the trend of Rebounds vs salary
-png(paste(PLOT_DIR, '/AgeAndPoints-Salary.png' , sep=''))
-#layout(matrix(c(1,2,3,4),2,2)) # optional 4 graphs/page 
-#plot(player_sal_reg)
-#abline(player_sal_reg)
-#dev.off()
-
-df = data.frame(player_salary$AGE, player_salary$PLAYER_SEASON_STATS_PTS, player_salary$salary)
-#dev.new()
-anova(player_sal_reg) # anova table
-qplot(player_salary$AGE, player_salary$salary, color=player_salary$PLAYER_SEASON_STATS_PTS, data=df)
-dev.off()
-######################################################################################################
-
-
-
-######################################################################################################
-#Player regresion analysis on Salary vs Position parameter
-player_salary <- read.csv("~/GP2/GP2/player_position.csv")
-player_salary$position <- factor(player_salary$position)
-player_sal_reg <- lm(player_salary$salary ~ player_salary$PLAYER_SEASON_STATS_PTS + 
-                        player_salary$PLAYER_SEASON_STATS_ST + 
-                          player_salary$position, player_salary)
-summary(player_sal_reg)
-
-#Plot the trend of Rebounds vs salary
-png(paste(PLOT_DIR, '/Exp-Position-Salary.png' , sep=''))
-df = data.frame(player_salary$experience, player_salary$position, player_salary$salary)
-qplot(player_salary$experience, player_salary$salary, color=player_salary$position, data=df)
 dev.off()
 ######################################################################################################
--- player_salary.sql
+++ player_salary.sql
@@ -1,10 +1,14 @@
 select tp.player_id, 
-       salary, 
        tp.team_code, 
        height, 
        experience, 
        age,
-       stats.*
+       stats.PLAYER_SEASON_STATS_AGE AGE,
+       sum(salary) salary,  
+       sum(PLAYER_SEASON_STATS_TRB) TRB,  
+       sum(PLAYER_SEASON_STATS_PTS) PTS,  
+       sum(PLAYER_SEASON_STATS_3P) THREE_P,  
+       sum(PLAYER_SEASON_STATS_FG) FG 
 from   player_season_stats stats, 
        team_salary tp, 
        player pl
@@ -12,4 +16,4 @@ where stats.PLAYER_SEASON_STATS_SEASON = tp.season
       and stats.player_id = pl.player_id
       and tp.season ='2010-11'
       and pl.player_id = tp.player_id
-      and tp.team_code = stats.player_season_stats_team_code;
+group by tp.player_id;
