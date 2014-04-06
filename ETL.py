b502ebd300915318d9dc2ddddda0aaa8d2f86293
Vinutha <vinutha@knorex.com>
2014-04-07 01:15:05 +0800

    Moving files inside sql folder

--- ETL.py
+++
@@ -1,5 +0,0 @@
-sqlite3 -header -csv Assignment.db < player_salary.sql > player_salary.csv
-
-sqlite3 -header -csv Assignment.db < player_position.sql > player_position.csv
-
-sqlite3 -header -csv Assignment.db < player_pos_count.sql > player_pos_count.csv
