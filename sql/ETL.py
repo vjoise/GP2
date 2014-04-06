cdcbc58eb1e126155932aa0193b85acf597ad8d6
Vinutha <vinutha@knorex.com>
2014-04-07 01:12:40 +0800

    Code structure reorg

---
+++ sql/ETL.py
@@ -0,0 +1,5 @@
+sqlite3 -header -csv Assignment.db < player_salary.sql > player_salary.csv
+
+sqlite3 -header -csv Assignment.db < player_position.sql > player_position.csv
+
+sqlite3 -header -csv Assignment.db < player_pos_count.sql > player_pos_count.csv
